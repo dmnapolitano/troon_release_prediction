980fff6ebe12aeedf5c67b52c311067d27a85a82
Napolitano, Diane <dnapolitano@ap.org>
2022-12-27 16:18:18 -0500

    It almost works :(

--- get_data_02.py
+++ get_data_02.py
@@ -1,19 +1,27 @@
 # Based on https://medium.com/swlh/tutorial-web-scraping-instagrams-most-precious-resource-corgis-235bf0389b0c
 # and https://github.com/jnawjux/web_scraping_corgis/blob/master/insta_scrape.py
 
+# THIS ALMOST WORKS
+# Log in to Instagram, exit Chrome, then
+# start Google Chrome from the command line with the `--remote-debugging-port=9222` argument.
+# The issue is that Instagram DRAMATICALLY changed the generated code per post :(
+
 from time import sleep
 import argparse
 from os.path import exists
 from html import unescape
 
-import chromedriver_binary
 from selenium.webdriver import Chrome
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
 import pandas
 from numpy import nan
 
 
 def get_data(url):
-    browser = Chrome()
+    options = Options()
+    options.add_experimental_option("debuggerAddress", "127.0.0.1:9222")
+    browser = Chrome(options=options)
     browser.get(url)
 
     likes = nan
@@ -23,14 +31,14 @@ def get_data(url):
 
     try:
         # This captures the standard like count. 
-        likes = browser.find_element_by_xpath(
+        likes = browser.find_element(By.XPATH,
             """//*[@id="react-root"]/section/main/div/div/
             article/div[2]/section[2]/div/div/button""").text.split()[0]
         likes = int(likes.replace(",", ""))
 
         age = browser.find_element_by_css_selector("a time").text
 
-        comment = unescape(browser.find_element_by_xpath(
+        comment = unescape(browser.find_element(By.XPATH,
             """//*[@id="react-root"]/section/main/div/div/
             article/div[2]/div[1]/ul/div/li/div/div/div[2]/span""").text)
     except:
