4925f7cb28205214c3b9b2167bc7f238d726e372
Napolitano, Diane <dnapolitano@ap.org>
2023-01-20 20:39:31 -0500

    More beer properties

--- get_beer_properties_04.py
+++ get_beer_properties_04.py
@@ -50,7 +50,7 @@ more_stopwords = ['amounts', 'especial', 'pink', 'weekend', 'amount', 'busiest',
                   'sexy', 'abdon', 'line', 'boatload', 'brett', 'room', 'dose', 'bludgeoningly',
                   'phenomenal', 'help', 'troon', 'huge', 'malted', 'everyone', 'application',
                   'primarily', 'batch', 'massive', 'oiliest', 'day', 'one', 'friends', 'monstrous',
-                  'many', 'friends', 'great']
+                  'many', 'friends', 'great', 'sorry', 'bottle', 'made']
 
 input_file = "troon_instagram_clean_post_data.csv"
 input_df = pandas.read_csv(input_file, index_col="id", dtype={"likes" : "Int64"})
